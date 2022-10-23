ac1067fe59ade5af729d40dd63ac676d0ce10bca
lyndseyumsted <lyndseyumsted@ucsb.edu>
2022-10-23 13:47:22 -0700

    Exploratory analysis

--- scripts/EDA.R
+++ scripts/EDA.R
@@ -6,9 +6,21 @@ pandemic_weekly_og$transit_change <- pandemic_weekly_og$transit_stations_percent
 pandemic_weekly_og$confirmed_cases <- pandemic_weekly_og$confirmed_cases_fill
 pandemic_weekly_og$new_cases <- pandemic_weekly_og$new_cases_fill
 
+pandemic_weekly_og %>%
+  group_by(ID_co) %>%
+  filter(week == "51") %>%
+  ggplot(aes(x = confirmed_cases)) +
+  geom_histogram(bins = 100) +
+  theme_bw()
+
+
+pandemic_new <- pandemic_weekly_og %>%
+  group_by(ID_co) %>%
+  filter(week == "51")
+
 library(dplyr)
-pandemic_weekly_sub <- pandemic_weekly_og %>%
-  select("ID_co", "week", "confirmed_cases", "new_cases", "population", "Mean_Tmax", "Urban", "Pop2010", 
+pandemic_weekly_sub <- pandemic_new %>%
+  select("ID_co", "confirmed_cases", "new_cases", "population", "Mean_Tmax", "Urban", "Pop2010", 
          "OHU2010", "LowIncomeTracts", "PovertyRate", "MedianFamilyIncome", "Ozone", "Diesel.PM", "Drinking.Water", "Pesticides", 
          "Traffic", "Groundwater.Threats", "Haz..Waste", "Solid.Waste", "Asthma", "Low.Birth.Weight", "Cardiovascular.Disease", 
          "Education", "Linguistic.Isolation", "Poverty", "Unemployment", "Pop..Char.", "under_10_.", "Age11_to_64_.", "over_65_.", 
@@ -16,6 +28,7 @@ pandemic_weekly_sub <- pandemic_weekly_og %>%
          "transit_change")
 
 library(finalfit)
+
 missing_plot(pandemic_weekly_sub)
 
 pandemic_weekly <- pandemic_weekly_sub %>%
@@ -24,7 +37,6 @@ pandemic_weekly <- pandemic_weekly_sub %>%
 missing_plot(pandemic_weekly)
 
 pandemic_weekly$ID_co <- as.factor(pandemic_weekly$ID_co)
-pandemic_weekly$week <- as.factor(pandemic_weekly$week)
 pandemic_weekly$population <- as.numeric(pandemic_weekly$population)
 
 str(pandemic_weekly)
@@ -37,6 +49,21 @@ View(a)
 
 #install.packages("corrplot")
 library(corrplot)
-pandemic_cor <- cor(pandemic_weekly[,-c(1,2)])
-corrplot(pandemic_cor, method = 'square')
-typeof(pandemic_weekly)
+pandemic_weekly[,-1] %>% 
+  select(is.numeric) %>% 
+  cor() %>% 
+  corrplot(diag = FALSE, 
+           method = 'square')
+
+
+summary(pandemic_weekly$confirmed_cases)
+hist(pandemic_weekly$confirmed_cases, breaks = 100)
+
+summary(pandemic_weekly$new_cases)
+hist(pandemic_weekly$new_cases, breaks = 100)
+
+pandemic_data <- subset(pandemic_weekly, 
+                        confirmed_cases > 20)
+
+hist(pandemic_data$confirmed_cases, include.lowest = TRUE)
+
